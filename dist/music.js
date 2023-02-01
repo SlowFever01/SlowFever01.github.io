9c2f0b5b8683137a233693b34c420687f1008edd
SlowFever01 <2093913128@qq.com>
2023-02-01 21:47:48 +0800

    Site updated: 2023-02-01 21:47:47

--- dist/music.js
+++ dist/music.js
@@ -12,10 +12,10 @@ const ap = new APlayer({
     // volume: 0.5, //默认音量
     audio: [{
         theme: 'pink',
-        name: '圈住你',
-        artist: '一口甜',
-        url: 'https://music.163.com/song/media/outer/url?id=1372552573',
-        cover: 'http://p1.music.126.net/Y05iUqwPp1IBwCj291_Ulg==/109951164156398679.jpg?param=130y130',
+        name: '有幸来过',
+        artist: '要不要买菜',
+        url: 'https://music.163.com/#/song?id=1458986027',
+        cover: 'http://p2.music.126.net/Cf7rCSKENcWSTCh3GDQfyg==/109951165099307945.jpg?param=130y130',
         lrc: ""
       }, {
         theme: '#46718b',
@@ -25,10 +25,10 @@ const ap = new APlayer({
         cover: 'http://p2.music.126.net/Ax-zrmAPBrASWxT92t3fdw==/109951164000242827.jpg?param=130y130',
       },
       {
-        name: "冬眠",
-        artist: '司南',
-        url: 'https://music.163.com/song/media/outer/url?id=1398663411',
-        cover: 'http://p2.music.126.net/4KDBaQXnQywQovmqvjx-8Q==/109951164444131697.jpg?param=130y130',
+        name: "断线",
+        artist: 'Shang / lil sophy',
+        url: 'https://music.163.com/#/song?id=496370620',
+        cover: 'http://p2.music.126.net/W-4b64hCb_s8OAGROQXgig==/109951162995598984.jpg?param=130y130',
         // theme: '#fff' //进度条颜色
       }
     ]
