6b863c2c5cae1342d1f9716f9b46f19374116ea8
SlowFever01 <2093913128@qq.com>
2023-02-02 09:50:36 +0800

    Site updated: 2023-02-02 09:50:36

--- dist/music.js
+++ dist/music.js
@@ -14,10 +14,11 @@ const ap = new APlayer({
         theme: 'pink',
         name: '有幸来过',
         artist: '要不要买菜',
-        url: 'https://music.163.com/#/song?id=1458986027',
+        url: 'https://music.163.com/song/media/outer/url?id=1458986027',
         cover: 'http://p2.music.126.net/Cf7rCSKENcWSTCh3GDQfyg==/109951165099307945.jpg?param=130y130',
         lrc: ""
-      }, {
+      }, 
+      {
         theme: '#46718b',
         name: '追',
         artist: '陈壹千',
@@ -27,10 +28,45 @@ const ap = new APlayer({
       {
         name: "断线",
         artist: 'Shang / lil sophy',
-        url: 'https://music.163.com/#/song?id=496370620',
+        url: 'https://music.163.com/song/media/outer/url?id=496370620',
         cover: 'http://p2.music.126.net/W-4b64hCb_s8OAGROQXgig==/109951162995598984.jpg?param=130y130',
-        // theme: '#fff' //进度条颜色
-      }
+        theme: '#fff' //进度条颜色
+      },
+      {
+        theme: '#46718b',
+        name: '心碎莫扎特',
+        artist: '暴躁的兔子',
+        url: 'https://music.163.com/song/media/outer/url?id=1900224553',
+        cover: 'http://p2.music.126.net/KOhv-eJTghAoNegrx2CIZw==/109951166689467629.jpg?param=130y130',
+      },
+      {
+        theme: '#46718b',
+        name: '我知道',
+        artist: 'WiFi歪歪',
+        url: 'https://music.163.com/song/media/outer/url?id=1979313985',
+        cover: 'http://p1.music.126.net/RzWnGoUOZtjiNvxzKortbA==/109951167853222659.jpg?param=130y130',
+      },
+      {
+        theme: '#46718b',
+        name: '愿你余生漫长',
+        artist: '王贰浪',
+        url: 'https://music.163.com/song/media/outer/url?id=1425814935',
+        cover: 'http://p1.music.126.net/x8mEAmSeYWPh-lXldUujmg==/109951164738889941.jpg?param=130y130',
+      },
+      {
+        theme: '#46718b',
+        name: '可不可以',
+        artist: '张紫豪',
+        url: 'https://music.163.com/song/media/outer/url?id=553755659',
+        cover: 'http://p2.music.126.net/WafK2OQfEtqXitdDXJ772Q==/109951163252847249.jpg?param=130y130',
+      },
+      {
+        theme: '#46718b',
+        name: 'Keep Your Head Up Princess',
+        artist: 'Anson Seabra',
+        url: 'https://music.163.com/song/media/outer/url?id=1886345442',
+        cover: 'http://p1.music.126.net/vUZstXP4G3tBXSGi5WbSDg==/109951166512266978.jpg?param=130y130',
+      },
     ]
   });
   
